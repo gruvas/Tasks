60bd9d3f8ccb84d477085d165d92248859ae11fa
gruvas <gruvas67@gmail.com>
2023-03-29 10:39:27 +0300

    Improved scripts for creating records in database tables

--- Tasks.PostgresMigrate/Scripts/2023-03-24-0001_CreateUsers.sql
+++ Tasks.PostgresMigrate/Scripts/2023-03-24-0001_CreateUsers.sql
@@ -1,11 +1,6 @@
-﻿INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Демидова', 'Вера', 'qweqw@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Александров', 'Тимофей', 'cas23@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Агапов', 'Тимофей', 'cwqe112@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Булатова', 'Анна', 'zxwq2@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Кузьмин', 'Андрей', 'd212d1sad@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Федоров', 'Артём', 'd12d21@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Давыдова', 'София', '1e2d@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Крючков', 'Максим', 'swqqw@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Киселева', 'Злата', 'dwq@gmail.com');
-INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Горюнова', 'Вероника', 'dwq@gmail.com');
+﻿INSERT INTO main."users" ("LastName", "FirstName", "Email") VALUES('Демидова', 'Вера', 'qweqw@gmail.com'),
+('Александров', 'Тимофей', 'cas23@gmail.com'), ('Агапов', 'Тимофей', 'cwqe112@gmail.com'), ('Булатова', 'Анна', 'zxwq2@gmail.com'),
+('Кузьмин', 'Андрей', 'd212d1sad@gmail.com'), ('Федоров', 'Артём', 'd12d21@gmail.com'), ('Давыдова', 'София', '1e2d@gmail.com'),
+('Крючков', 'Максим', 'swqqw@gmail.com'),('Киселева', 'Злата', 'dwq@gmail.com'),('Кудрявцева', 'Дарья', 'kd@gmail.com'),
+('Шишкина', 'Ясмина', 'shaz@gmail.com'),('Фокина', 'Анастасия', 'fokA@gmail.com'),('Артамонова', 'Екатерина', 'ArtEk@gmail.com');
 
